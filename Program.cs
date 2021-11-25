8f79e07b3902e4f65a452e808ccc30ff7cad603a
Leandro Junior <leandrojr70@gmail.com>
2021-11-25 10:55:12 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            Console.WriteLine("Digite o valor para verificação");
+            valor = int.Parse(Console.ReadLine());
+
+            if (valor % 2 == 0)
+            {
+                Console.WriteLine("Número par");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+        }
+    }
+}
